d5f1beaba14890e0f39a3ec1e0408cc3c11ac69f
123Sabina-V <lelichek2002@gmail.com>
2019-11-06 10:50:04 +0300

    v1.0

---
+++ lr 6-1/lr 6-1.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<Windows.h>
+#include<math.h>
+
+using namespace std;
+int main()
+{
+	int x, n = 10;
+	cout << "Enter x=";
+	cin >> x;
+	float rez;
+	int sum = 0;
+	for (int k = 1; k < 11; k++)
+	{
+		sum = sum + k;
+	}
+	rez = (pow(x, 2)) / (pow(x, 2) + 2) + sum;
+	cout << rez << endl;
+	system("pause");
+	return 0;
+ }
